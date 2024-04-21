a3b47f77696a5707f05332ab6a44cec3778912e7
1nnsane <dj.diko1337@gmail.com>
2024-04-20 23:04:14 -0500

    lesson 11

---
+++ lesson11-20.c
@@ -0,0 +1,51 @@
+//Урок 10 (C user input) Плохо понял, разобрать еще
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>  //с урок 18
+
+
+
+
+int main(){
+/*
+    char name[25]; //массив 25 байт
+    int age;
+
+    printf ("what ur name\n");
+    //scanf ("%s", &name); (читает только одно слово)
+    fgets(name, 25,stdin); //читает всю строку, можно вписывать несколько слов
+    name[strlen(name)-1] = '\0'; //отключает перенос принтф после кода - строки 10 
+
+
+    printf("How old are you?\n");
+    scanf("%d", &age);
+
+    printf("hello %s, how are u\n",name);
+    printf("You are %d years old\n", age);
+*/
+
+//Урок 11 про мат.библиотеку, перескочил, нужно вернуться потом
+/*
+    double A = sqrt(9);
+    double B = pow(2, 4);
+    int C = round(3.14);
+    int D = ceil(3.14);
+    int E = floor(3.99);
+    double F = fabs(-100);
+    double G = log(3);
+    double H = sin(45);
+    double I = cos(45);
+    double J = tan(45);
+    
+    printf("\n%lf", A); 
+*/
+
+
+
+
+
+return 0;
+}
