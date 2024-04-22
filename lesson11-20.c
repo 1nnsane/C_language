7628fdd3bc3965ad64d2a685ab44a9daa587d5f4
1nnsane <dj.diko1337@gmail.com>
2024-04-21 23:24:58 -0500

    13 lesson

--- lesson11-20.c
+++ lesson11-20.c
@@ -62,6 +62,20 @@ printf ("circumference: %lf\n", circumference);
 printf ("area: %lf\n", area);
 */
 
+//Урок 13 (нахождение гипотенузы)
+double a;
+double b;
+double c;
+
+printf ("Enter side a ");
+scanf ("%lf", &a);
+
+printf ("Enter side b ");
+scanf ("%lf", &b);
+
+c = sqrt (a*a + b*b);
+printf ("Side C %lf", c);
+
 
 
 return 0;
