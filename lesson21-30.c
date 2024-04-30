3ef3dad5bc23fe22d03d172d9856d2297de791cd
1nnsane <dj.diko1337@gmail.com>
2024-04-30 18:41:16 -0500

    lesson 22

--- lesson21-30.c
+++ lesson21-30.c
@@ -4,7 +4,6 @@
 
 //Урок 21 (С functions 📞)
 //создание функции void, для повтора команды, вместо того чтобы писать 12 раз printf
-
 void birthday () //21 Урок
 {
     printf("Happy Birthday\n");
@@ -13,10 +12,30 @@ void birthday () //21 Урок
     printf("Happy Birthday to you\n");
 }
 
+//Урок 22 
+//функция int не может видеть данные с функции void
+//надо вписать код как на строке 37, и ввести данные функций name age B int, void = (с типом данных char/int) (26,37 строки)
+void birthday (char name[],int age)
+{
+printf("\nHappy birthday dear %s", name); //строка 33
+printf("\nNow you a %d years old!", age); //строка 34
+}
+
 int main()
 {
+    /* //lesson 21
     birthday ();
     birthday ();
     birthday (); //повторение команды void (8-11 строки)
+    */
+
+    char name [] = "Diyar";
+    int age = 21;
+
+    birthday(name,age);
+
+
+
+
     return 0;
 }
