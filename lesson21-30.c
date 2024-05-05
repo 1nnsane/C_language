af0e89219f384c288dff40ecfdc91fbee83197f9
1nnsane <dj.diko1337@gmail.com>
2024-05-04 23:14:31 -0500

    lesson 26

--- lesson21-30.c
+++ lesson21-30.c
@@ -122,6 +122,28 @@ int main()
    printf("%s", string1);
 
 
+   // 2 часть кода
+
+   char string1[] = "Bro";
+   char string2[] = "Code";
+
+   int result = strlen(string1);                          // returns string length as int
+   //int result = strcmp(string1, string2);         // string compare all characters
+   //int result = strncmp(string1, string2, 1);   // string compare n characters
+   //int result = strcmpi(string1, string1);        // string compare all (ignore case)
+   //int result = strnicmp(string1, string1, 1);  // string compare n characters (ignore case)
+
+   printf("%d", result);
+
+
+   if(result == 0)
+   {
+      printf("These strings are the same");
+   }
+   else
+   {
+      printf("These strings are not the same");
+   }
 
 
    
