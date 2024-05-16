0fe69d9f9dcda056d76d8aa7b08a4388b6cf6c98
1nnsane <dj.diko1337@gmail.com>
2024-05-16 15:47:38 -0500

    lesson 33

--- lesson21-30.c
+++ lesson21-30.c
@@ -254,6 +254,15 @@ int main()
    printf("$%.2lf", prices[0]);
    */
 
+   
+   //Урок 33 (C print an array with loop 🔃)
+   double prices[] = {5.0, 10.0, 15.0, 25.0, 20.0, 30.0};
+   //printf("%d bytes\n", sizeof(prices));
+   for(int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++)
+   {
+      printf("$%.2lf\n", prices[i]);
+   }
+
    return 0;
 }
 
