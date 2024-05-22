24627f0e0374e572bc2904ceb651e645520a64e9
1nnsane <dj.diko1337@gmail.com>
2024-05-21 23:11:10 -0500

    lesson 34

--- lesson21-30.c
+++ lesson21-30.c
@@ -256,15 +256,54 @@ int main()
 
    
    //Урок 33 (C print an array with loop 🔃)
+   /*
    double prices[] = {5.0, 10.0, 15.0, 25.0, 20.0, 30.0};
    //printf("%d bytes\n", sizeof(prices));
    for(int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++)
    {
       printf("$%.2lf\n", prices[i]);
    }
+   */
+
+   
+   //Lesson 34
+   // 2D array = an array, where each element is an entire array
+   //            useful if you need a matrix, grid, or table of data
+   
+   //   int numbers[2][3] = {
+   //                        {1, 2, 3},
+   //                        {4, 5, 6}
+   //                       };
+   
+   int numbers[2][3];
+
+   int rows = sizeof(numbers)/sizeof(numbers[0]);
+   int columns = sizeof(numbers[0])/sizeof(numbers[0][0]);
+
+   printf("rows: %d\n", rows);
+   printf("columns: %d\n", columns);
+
+   numbers[0][0] = 1;
+   numbers[0][1] = 2;
+   numbers[0][2] = 3;
+   numbers[1][0] = 4;
+   numbers[1][1] = 5;
+   numbers[1][2] = 6;
+
+   for(int i = 0; i < rows; i++)
+   {
+      for(int j = 0; j < columns; j++)
+      {
+         printf("%d ", numbers[i][j]);
+      }
+      printf("\n");
+   }
+
 
    return 0;
 }
+
+   
 
 //lesson 25 (prototype)
 void hello(char name[], int age)
