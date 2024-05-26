9112bb1a8ecd7f17fa53dacc2e410bbd79968643
1nnsane <dj.diko1337@gmail.com>
2024-05-25 23:01:26 -0500

    lesson 35

--- lesson21-30.c
+++ lesson21-30.c
@@ -299,7 +299,15 @@ int main()
       printf("\n");
    }
 
+   char cars[][10] = {"Mustang","Corvette","Camaro"};
+   //cars[0] = "Tesla";
+   strcpy(cars[0], "Tesla");
+   for(int i = 0; i < sizeof(cars)/sizeof(cars[0]); i++)
+   {
+      printf("%s\n", cars[i]);
+   }
 
+   
    return 0;
 }
 
