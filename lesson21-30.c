b1797ab3c3384a9b5683c2200f40658598c85083
1nnsane <dj.diko1337@gmail.com>
2024-09-19 23:27:13 -0500

    lesson 36

--- lesson21-30.c
+++ lesson21-30.c
@@ -303,19 +303,25 @@ int main()
 
 
    //урок 35 (C array of strings🧵)
+//<string.h>
+/*
+int main()
+{
    char cars[][10] = {"Mustang","Corvette","Camaro"};
+
    //cars[0] = "Tesla";
    strcpy(cars[0], "Tesla");
+
    for(int i = 0; i < sizeof(cars)/sizeof(cars[0]); i++)
    {
       printf("%s\n", cars[i]);
    }
 
-
    return 0;
 }
+*/
 
-   
+}
 
 //lesson 25 (prototype)
 void hello(char name[], int age)
